3cd163d0240be40e7941efd55de9f6e12b5e208d
veraprime <143101659+veraprime@users.noreply.github.com>
2023-10-11 19:58:37 -0400

    Update prometheus-setup.sh

--- prometheus-setup.sh
+++ prometheus-setup.sh
@@ -20,7 +20,7 @@ sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin/
 
 rm -rf prometheus-2.23.0.linux-amd64.tar.gz prometheus-2.19.0.linux-amd64
 # setting up the dependencies from the repo setup
-sudo git clone -b installations https://github.com/abdelazziz20/devops-fully-automated.git /tmp/devops-fully-automated
+sudo git clone -b main https://github.com/veraprime/devops-fully-automated.git /tmp/devops-fully-automated
 sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/prometheus.yml /etc/prometheus/
 sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/prometheus.service /etc/systemd/system/prometheus.service
 
@@ -31,6 +31,85 @@ sudo chown -R prometheus:prometheus /etc/prometheus/consoles
 sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
 sudo chown -R prometheus:prometheus /var/lib/prometheus
 
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
+sudo systemctl status prometheus
+
+
+or
+
+#!/bin/bash
+
+# Prerequisites check
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root or with sudo"
+  exit
+fi
+
+# Ensure the EC2 instance is up & running and you're able to SSH
+# Ensure port 9090 is open for Prometheus traffic
+
+# Step 1: EC2 should already be running. This script starts from Step 2.
+
+# Step 2: Install Prometheus
+
+# Create a user and directories for Prometheus
+sudo useradd --no-create-home prometheus
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+
+# Download and install Prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.37.0/prometheus-2.37.0.linux-amd64.tar.gz
+tar xvfz prometheus-2.37.0.linux-amd64.tar.gz
+sudo cp prometheus-2.37.0.linux-amd64/prometheus /usr/local/bin/
+sudo cp prometheus-2.37.0.linux-amd64/promtool /usr/local/bin/
+sudo cp -r prometheus-2.37.0.linux-amd64/consoles /etc/prometheus
+sudo cp -r prometheus-2.37.0.linux-amd64/console_libraries /etc/prometheus
+rm -rf prometheus-2.37.0.linux-amd64.tar.gz prometheus-2.37.0.linux-amd64
+
+# Configure Prometheus
+cat << EOF > /etc/prometheus/prometheus.yml
+global:
+  scrape_interval: 15s
+  external_labels:
+    monitor: 'prometheus'
+scrape_configs:
+  - job_name: 'prometheus'
+    static_configs:
+      - targets: ['localhost:9090']
+EOF
+
+# Setup Prometheus as a service
+cat << EOF > /etc/systemd/system/prometheus.service
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Update permissions
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo chown -R prometheus:prometheus /var/lib/prometheus
+
+# Reload systemd and enable Prometheus service
 sudo systemctl daemon-reload
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
