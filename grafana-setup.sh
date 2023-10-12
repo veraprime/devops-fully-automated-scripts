654ff9b608e21d207a59bc8593faed63fb6e03ab
veraprime <143101659+veraprime@users.noreply.github.com>
2023-10-11 20:04:57 -0400

    Update grafana-setup.sh

--- grafana-setup.sh
+++ grafana-setup.sh
@@ -17,7 +17,7 @@ sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_export
 rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
 
 # setup the node-exporter dependencies
-sudo git clone -b installations https://github.com/abdelazziz20/devops-fully-automated.git /tmp/devops-fully-automated
+sudo git clone -b main https://github.com/veraprime/devops-fully-automated.git /tmp/devops-fully-automated
 sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/node-exporter.service /etc/systemd/system/node-exporter.service
 
 sudo systemctl daemon-reload
